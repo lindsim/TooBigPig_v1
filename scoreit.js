5bdacd5dcbd803e1de3b21430bec93de64ab12c9
lindsim <lindsey@localhost.(none)>
2015-09-15 18:06:06 -0700

    fixing js

---
+++ scoreit.js
@@ -0,0 +1,41 @@
+var ref = new Firebase("https://too-big-pig-1.firebaseio.com/scores");
+var items = [];
+
+ref.orderByChild("name").once("value", function(snapshot) {
+	snapshot.forEach(function(data){
+		items.push([data.val().points, data.val().name]);
+	});
+
+
+
+function compareNumbers (a, b) {
+  return a[0] < b[0];
+}
+
+var itemsSort = items.sort(compareNumbers);
+
+var display = [];
+for (var x = 0; x < 10; x++){
+	display.push("<li>" + itemsSort[x][1] + "....." + itemsSort[x][0] + "</li>");
+}
+	$(".ordered").append(display.join(''));
+
+});
+
+
+$(document).ready(function(){
+	$('#pigface').click(swapFace);
+});
+
+var pig = "images/pigface.png";
+
+var swapFace = function(){
+	if (pig == "images/pigface.png"){
+		pig = "images/winkpig.png";
+	}
+	else if (pig == "images/winkpig.png"){
+		pig = "images/pigface.png";
+	}
+
+	$(this).attr('src', pig);
+}
