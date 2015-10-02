c28f43f5247cc6d13c0c42934cba656e070fcd30
lindsim <lindsey@localhost.(none)>
2015-10-01 18:38:01 -0700

    still cleaning up js

---
+++ swap.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+	$('#pigface').click(swapFace);
+});
+
+var pig = "images/pigface.png";
+
+var swapFace = function(){
+	if (pig == "images/pigface.png"){
+		pig = "images/winkpig.png";
+	}
+	else if (pig == "images/winkpig.png"){
+		pig = "images/pigface.png";
+	}
+
+	$(this).attr('src', pig);
+}
